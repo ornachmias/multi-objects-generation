03c0ee9ed6f4f6c5129695e8650e2a9b64460730
Or Nachmias <or.nachmias@gmail.com>
2020-12-19 14:38:45 +0200

    Modified main according to new script.

--- generate_v2.py
+++ generate_v2.py
@@ -4,11 +4,14 @@ from data_generation.scenes_3d_bbox_render_transform import Scenes3DBboxRenderTr
 
 parser = argparse.ArgumentParser(description='Data Generation Tool V2')
 parser.add_argument('-d', '--data_dir', default='./data')
-parser.add_argument('-t', '--type', default='', choices=['scenes_3d_bbox_render_transform'])
+parser.add_argument('-t', '--type', default='', choices=['scenes_3d_bbox_render_transform',
+                                                         'scenes_3d_bbox_render_random'])
 args = parser.parse_args()
 
 generator = None
 if args.type == 'scenes_3d_bbox_render_transform':
+    generator = Scenes3DBboxRenderTransform(args.data_dir)
+elif args.type == 'scenes_3d_bbox_render_random':
     generator = Scenes3DBboxRenderTransform(args.data_dir)
 
 generator.initialize()
